7b080f3ec6dad4d1d1ecc85034f9a5cc71cc4a41
Guy John <guy@rumblesan.com>
2015-12-02 13:47:57 +0000

    initial mandelbrot is correct ratio

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,24 @@ var React = require('react');
 
 var App = require('./app.jsx');
 
+var canvasWidth  = document.documentElement.clientWidth;
+var canvasHeight = document.documentElement.clientHeight;
+var hwRatio = canvasHeight / canvasWidth;
+var plotWidth = 3;
+var centre = {
+    x: -0.5,
+    y: 0
+}
+var plotHeight = plotWidth * hwRatio;
+
 var config = {
-    width: document.documentElement.clientWidth,
-    height: document.documentElement.clientHeight,
+    width: canvasWidth,
+    height: canvasHeight,
     repeats: 255,
-    startX1: -2.0,
-    startY1: -1.0,
-    startX2:  1.0,
-    startY2:  1.0,
+    startX1: centre.x - (plotWidth / 2),
+    startY1: centre.y - (plotHeight / 2),
+    startX2: centre.x + (plotWidth / 2),
+    startY2: centre.y + (plotHeight / 2),
     colours: {
         hueOffset: 0.63,
         saturation: 0.9,
